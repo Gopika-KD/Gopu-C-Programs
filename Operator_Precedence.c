a42537bc963844360222f6ef8e68bf74894ce44e
Gopika-KD <gopikakd8@gmail.com>
2025-06-29 17:50:25 +0530

    Precedence Concept

---
+++ Operator_Precedence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    // Variables a,b,c,d
+    int a = 5 * 2 - 2 * 3;
+    int b = 5 * 2 / 2 * 3;
+    int c = 5 * (2 / 2) * 3;
+    int d = 5 + 2 / 2 * 3;
+
+    // Prints the output
+    printf("The value of a = %d\n", a);
+    printf("The value of b = %d\n", b);
+    printf("The value of c = %d\n", c);
+    printf("The value of d = %d\n", d);
+
+    return 0;
+}
